8c8345d52ad666a7d55d845441d3d95caaf7bf54
junghongseop <a01082372487@gmail.com>
2024-07-17 11:07:24 +0900

    add: 아이콘 추가

---
+++ src/icon/Background.jsx
@@ -0,0 +1,20 @@
+const Background = () => {
+  return (
+    <svg
+      width="1510"
+      height="840"
+      viewBox="0 0 1510 840"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0 673.284L125.833 721.376C251.667 769.468 503.333 865.651 755 833.59C1006.67 801.529 1258.33 784.77 1384.17 704.617L1510 619.723V0H1384.17C1258.33 0 1006.67 0 755 0C503.333 0 251.667 0 125.833 0H0V673.284Z"
+        fill="#E6E6FA"
+      />
+    </svg>
+  );
+};
+
+export default Background;
---
+++ src/icon/IconLayer.jsx
@@ -0,0 +1,12 @@
+const IconLayer = () => {
+  return (
+    <svg viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M0 27V13.5H13.5V27H0ZM16.875 20.25V10.125H6.75V6.75H20.25V20.25H16.875ZM23.625 13.5V3.375H13.5V0H27V13.5H23.625Z"
+        fill="#6C757D"
+      />
+    </svg>
+  );
+};
+
+export default IconLayer;
